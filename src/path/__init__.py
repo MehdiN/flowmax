c3d5af9c8f9ac7593e27b4ef025213c4333f2f2d
nmehdi <nmehdi@noreply.codeberg.org>
2026-06-09 01:29:20 +0200

    add __init__

---
+++ src/path/__init__.py
@@ -0,0 +1,11 @@
+from .affine import AffineProbPath, CondOTProbPath
+from .path import BaseProbPath
+from .path_sample import DiscretePathSample, PathSample
+
+__all__ = [
+    "BaseProbPath",
+    "AffineProbPath",
+    "CondOTProbPath",
+    "PathSample",
+    "DiscretePathSample",
+]
