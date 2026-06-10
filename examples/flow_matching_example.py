78acfbc6756d6e0d81c4c986d0b020040000922d
nmehdi <nmehdi@noreply.codeberg.org>
2026-06-10 14:11:00 +0200

    refactor: update flow matching example to visualize ODE trajectories

--- examples/flow_matching_example.py
+++ examples/flow_matching_example.py
@@ -9,6 +9,7 @@ import diffrax as dfx
 import optax
 from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
+from einops import rearrange
 
 class Flow(eqx.Module):
     net: eqx.nn.Sequential
@@ -56,21 +57,18 @@ def make_step(model, x_1, key, opt_state, opt_update):
     key = jxr.split(key)[0]
     return loss, model, key, opt_state
 
+# the sampling function by solving the ODE
 @eqx.filter_jit
-def single_step_fn(model, x_t, t_0, t_1):
-    t_2 = (t_1 - t_0)/2
-    return x_t +(t_1 - t_0) * model(x_t + model(x_t, t_0) * t_2, t_0 + t_2)
-
-@eqx.filter_jit
-def single_sample_fn(model,dt0,key):
+def single_sample_fn(model,dt0,num_steps,key)->jax.Array:
     def flow(t,x, args):
         return model(x,t)
     term = dfx.ODETerm(flow)
     solver = dfx.Tsit5()
     x0=jxr.normal(key,(2,))
-    # jax.debug.breakpoint()
-    sol = dfx.diffeqsolve(term,solver,t0=0,t1=1,dt0=dt0,y0=x0)
-    return sol.ys[0]
+    timegrid = jnp.linspace(0,1,num_steps)
+    saveat = dfx.SaveAt(t0=False,t1=False,ts=timegrid)
+    sol = dfx.diffeqsolve(term,solver,t0=0,t1=1,dt0=dt0,y0=x0,saveat=saveat)
+    return sol.ys
 
 def main():
 
@@ -89,7 +87,7 @@ def main():
         n+=1
         if step % 100 == 0:
             # jax.debug.breakpoint()
-            print(f"Loss={total_loss/n}")
+            print(f"Loss={(total_loss/n):.6f}")
             n = 0
             total_loss = 0
 
@@ -99,36 +97,23 @@ def main():
     n_steps = 8
     timesteps = jnp.linspace(0,1.0,n_steps+1)
 
-    # fig, ax = plt.subplots()
-    # xa = make_moons(256,noise=0.05)[0]
-    # ax.scatter(xa[:,0],xa[:,1])
-
-    fig, axes = plt.subplots(1, n_steps + 1, figsize=(30, 4), sharex=True, sharey=True)
-
-    axes[0].scatter(x[:, 0], x[:, 1], s=10)
-    axes[0].set_title(f't = {timesteps[0]:.2f}')
-    axes[0].set_xlim(-3.0, 3.0)
-    axes[0].set_ylim(-3.0, 3.0)
-    
-    
-    for i in range(n_steps):
-        t0 = timesteps[i]
-        t1 = timesteps[i+1]
-        step_fn = ft.partial(single_step_fn,model,t_0=t0,t_1=t1)
-        x = jax.vmap(step_fn)(x)
-        # jax.debug.breakpoint()
-        axes[i + 1].scatter(x[:, 0], x[:, 1], s=10)
-        axes[i + 1].set_title(f't = {timesteps[i + 1]:.2f}')
-
-    fig,ax2= plt.subplots()
     num_samples = 300
     sample_keys = jxr.split(subkey,num_samples)
     data_shape = (num_samples, 2)
-    sample_fn = ft.partial(single_sample_fn,model,0.1)
+    n_steps=6
+    timesteps = jnp.linspace(0,1,n_steps) 
+    sample_fn = ft.partial(single_sample_fn,model,0.1,n_steps)
     samples = jax.vmap(sample_fn)(sample_keys)
-    ax2.scatter(samples[:, 0], samples[:, 1], s=10)
+    samples = rearrange(samples,"n b d ->b n d")
+    fig, axes = plt.subplots(1, n_steps, figsize=(30, 4), sharex=True, sharey=True)
+    
+    axes[0].set_xlim(-3.0, 3.0)
+    axes[0].set_ylim(-3.0, 3.0)
+    for i in range(n_steps):
+        axes[i ].scatter(samples[i,:, 0], samples[i,:, 1], s=10)
+        axes[i].set_title(f't = {timesteps[i]:.2f}')
 
-    plt.tight_layout()
+    fig.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
