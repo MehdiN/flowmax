698dbac3dd2e04bb9cb1cdfaf62d62da817af6c8
nmehdi <nmehdi@noreply.codeberg.org>
2026-06-10 14:12:32 +0200

    refactor(path): register BaseProbPath as JAX PyTree node

--- flowmax/path/path.py
+++ flowmax/path/path.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 import jax
+from .scheduler import Scheduler
 from .path_sample import PathSample
 
+@jax.tree_util.register_pytree_node_class
 class BaseProbPath(ABC):
+    scheduler: Scheduler
 
     """
     Intended usage
@@ -14,12 +17,28 @@ class BaseProbPath(ABC):
         ## optimize
 
     """
+    def __init__(self, scheduler) -> None:
+        super().__init__()
+        self.scheduler = scheduler
+
     @abstractmethod
     def sample(self,x_0:jax.Array, x_1:jax.Array, t: jax.Array, *, key) -> PathSample:
         """
            TODO: add doc
         """
     ...
+
+    # to register the path as a Pytree
+    def tree_flatten(self):
+        children = None
+        aux_data = {"scheduler": self.scheduler}
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, chidren):
+        return cls(*chidren, **aux_data)
+
+
     # not useful for jax code ?
     # def assert_sample_shape(self, x_0: jax.Array, x_1: jax.Array, t: jax.Array):
     #     assert (
